fc3c691f0084532bacff25e0f0a32ac6180bc853
Travis Staloch <twostepted@gmail.com>
2019-12-04 05:31:55 -0800

    day 4 ~6x speedup using diffs

--- 4.zig
+++ 4.zig
@@ -7,28 +7,40 @@ pub fn main() anyerror!void {
     var part1: i32 = 0;
     var part2: i32 = 0;
     var n: i32 = 245182;
-    var buf: [20]u8 = undefined;
-    // var buf: [20]i8 = undefined;
+    var digits: [20]i8 = undefined;
     while (n <= 790572) : (n += 1) {
-        const s = try std.fmt.bufPrint(&buf, "{d}", n);
-        var inc = true;
-        var consec1 = false;
-        var consec2 = false;
-        for (s[0 .. s.len - 1]) |c, si| {
-            if (c > s[si + 1]) {
-                inc = false;
-                break;
+        var i: usize = 0;
+        var x = n;
+        while (x > 0) : (x = @divTrunc(x, 10)) {
+            digits[i] = @truncate(i8, @mod(x, 10));
+            i += 1;
+        }
+        var diffs = digits[0..i];
+        std.mem.reverse(i8, digits[0..i]);
+        for (diffs[0 .. diffs.len - 1]) |diff, di| {
+            diffs[di] = diffs[di + 1] - diff;
+        }
+        var is_inc = true;
+        var is_consec1 = false;
+        var is_consec2 = false;
+        for (diffs) |diff, di| {
+            if (diff < 0) {
+                is_inc = false;
+                if (is_consec1 and is_consec2) break;
+            }
+            if (diff == 0) {
+                is_consec1 = true;
+                if ((di == 0 or diffs[di - 1] != 0) and
+                    (di == diffs.len - 1 or diffs[di + 1] != 0))
+                {
+                    is_consec2 = true;
+                    if (!is_inc) break;
+                }
             }
         }
 
-        for ("0123456789") |c| {
-            const two_consec = std.mem.indexOf(u8, s, &[_]u8{ c, c }) != null;
-            if (two_consec) consec1 = true;
-            if (two_consec and std.mem.indexOf(u8, s, &[_]u8{ c, c, c }) == null) consec2 = true;
-            if (consec1 and consec2) break;
-        }
-        if (inc and consec1) part1 += 1;
-        if (inc and consec2) part2 += 1;
+        if (is_inc and is_consec1) part1 += 1;
+        if (is_inc and is_consec2) part2 += 1;
     }
     warn("part1: {} part2: {}\n", part1, part2);
 }
